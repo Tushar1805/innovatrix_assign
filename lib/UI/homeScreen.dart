45fde8fb61b335d15d233d0cdce46afa3a77cec5
Tushar Kalbhande <tusharkalbhande18@gmail.com>
2024-04-15 11:05:14 +0530

    GraphQL connected

--- lib/UI/homeScreen.dart
+++ lib/UI/homeScreen.dart
@@ -1,20 +1,92 @@
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: Container(
-        color: Colors.grey.shade100,
-        child: Text("Logout"),
-      ),
-      appBar: AppBar(
-        title: Text("GraphQL Data"),
-      ),
-      body: Container(
-        child: Text("Hello"),
+    final HttpLink httpLink = HttpLink(
+      'https://countries.trevorblades.com/',
+    );
+
+    ValueNotifier<GraphQLClient> client = ValueNotifier<GraphQLClient>(
+      GraphQLClient(
+          link: httpLink, cache: GraphQLCache(dataIdFromObject: null)),
+    );
+    return GraphQLProvider(
+      client: client,
+      child: Scaffold(
+        drawer: Container(
+          color: Colors.grey.shade100,
+          child: Text("Logout"),
+        ),
+        appBar: AppBar(
+          title: Text("GraphQL Data"),
+        ),
+        body: Query(
+          options: QueryOptions(
+            document: gql(r""" 
+         query ReadCountries {
+                  countries {
+                    code
+                    name
+                    currency
+                    emoji
+                  }
+              }
+          """),
+          ),
+          builder: (QueryResult result,
+              {VoidCallback? refetch, FetchMore? fetchMore}) {
+            if (result.hasException) {
+              return Text(result.exception.toString());
+            }
+
+            if (result.isLoading) {
+              return const Text('Loading');
+            }
+
+            List? repositories = result.data?['countries'];
+
+            if (repositories == null) {
+              return const Text('No repositories');
+            }
+
+            return ListView.builder(
+              itemCount: repositories.length,
+              itemBuilder: (context, index) {
+                final repository = repositories[index];
+                return Card(
+                  elevation: 5,
+                  margin: EdgeInsets.all(10),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  color: Colors.deepPurple.shade200,
+                  child: ListTile(
+                    leading: Text(
+                      repository["emoji"],
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    title: Text(
+                      repository["name"],
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    subtitle: Text(
+                      'Currency: ${repository["currency"]}',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    trailing: Text(
+                      repository["code"],
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                );
+              },
+            );
+          },
+        ),
       ),
     );
   }
